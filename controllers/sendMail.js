f92ed3292f11ac8975ac72a634f9285a07e0ebcd
Nicholas Wootton <nicholas.wootton@insurancerevolution.co.uk>
2024-06-17 15:50:57 +0100

    add debug object into response from /sendmail

--- controllers/sendMail.js
+++ controllers/sendMail.js
@@ -27,7 +27,9 @@ export const sendMail = async (req, res) => {
 
   try {
     await transport.sendMail(mailOptions);
-    res.status(200).json({ status: 200, messaage: "Email sent succesfully" });
+    res
+      .status(200)
+      .json({ status: 200, debugObject, messaage: "Email sent succesfully" });
   } catch (err) {
     console.error(err, "error sending email");
     res.status(500).json({
