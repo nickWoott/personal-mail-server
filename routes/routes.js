bf960ed42c5868924c5d9595ac5ee864ab096431
Nicholas Wootton <nicholas.wootton@insurancerevolution.co.uk>
2024-06-17 16:00:02 +0100

    add in authentication middleware

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
 import { healthcheck } from "../controllers/healthcheck";
 import { sendMail } from "../controllers/sendMail";
+import { authenticateToken } from "../middleware/auth";
 
 const router = Router();
 
 router.get("/healthcheck", healthcheck);
-router.post("/sendMail", sendMail);
+router.post("/sendMail", authenticateToken, sendMail);
 
 export default router;
