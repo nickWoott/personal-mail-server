b476a879a747906a6839a6176af58c6db8055372
Nicholas Wootton <nicholas.wootton@insurancerevolution.co.uk>
2024-07-16 15:41:08 +0100

    removed unused imports

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,8 +1,4 @@
-import express, { Router } from "express";
 import serverless from "serverless-http";
-import router from "../../routes/routes";
 import app from "../../server";
 
 export const handler = serverless(app);
-
-
